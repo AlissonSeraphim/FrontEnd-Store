b22bf6175905a00705349db290e09e9ac1d163e8
Glayson <glaysonbss@gmail.com>
2023-03-17 17:42:41 -0300

    fix: lint error fixed on req 04

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getCategories } from '../services/api';
 import Categories from '../components/Categories';
-import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   constructor() {
