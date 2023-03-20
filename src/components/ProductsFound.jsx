cbe596fc3b4b41abcd2d94623a54cf06753a6650
MatheusTrein <matheusptrein@hotmail.com>
2023-03-20 15:36:12 -0300

    feat: product page

--- src/components/ProductsFound.jsx
+++ src/components/ProductsFound.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductsFound extends React.Component {
   render() {
@@ -7,13 +8,16 @@ class ProductsFound extends React.Component {
       productName,
       productImg,
       productPrice,
+      productId,
     } = this.props;
 
     return (
       <div className="ProductsCard" data-testid="product">
-        <h3>{productName}</h3>
-        <img src={ productImg } alt={ productName } />
-        <p>{productPrice}</p>
+        <Link to={ `/product/${productId}` } data-testid="product-detail-link">
+          <h3>{productName}</h3>
+          <img src={ productImg } alt={ productName } />
+          <p>{productPrice}</p>
+        </Link>
       </div>
     );
   }
@@ -23,6 +27,7 @@ ProductsFound.propTypes = {
   productName: PropTypes.string.isRequired,
   productImg: PropTypes.string.isRequired,
   productPrice: PropTypes.number.isRequired,
+  productId: PropTypes.string.isRequired,
 };
 
 export default ProductsFound;
